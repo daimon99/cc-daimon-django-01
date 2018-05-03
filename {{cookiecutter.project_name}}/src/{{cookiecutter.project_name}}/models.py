4fe7c312896eb5d2552228efdc12496203afbfae
daijian@air <daijian@flowself.com>
2018-05-03 18:48:01 +0800

    fix bug

--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/models.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 # Create your models here.
 
-class PathRule(models.Model):
-    path = models.CharField(max_length=100)
-    rv = models.CharField(max_length=500)
-    content_type = models.CharField(max_length=50, default='application/json')
+# class PathRule(models.Model):
+#     path = models.CharField(max_length=100)
+#     rv = models.CharField(max_length=500)
+#     content_type = models.CharField(max_length=50, default='application/json')
