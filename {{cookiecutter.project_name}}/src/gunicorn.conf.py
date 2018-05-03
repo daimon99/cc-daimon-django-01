691ff8b6972c91fe9a02d9c6f0a9301e487c35a7
daijian@air <daijian@flowself.com>
2018-05-03 14:09:48 +0800

    fix bugs

--- {{cookiecutter.project_name}}/src/gunicorn.conf.py
+++ {{cookiecutter.project_name}}/src/gunicorn.conf.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
-bind = "127.0.0.1:9002"
+bind = "127.0.0.1:{{cookiecutter.port}}"
 workers = multiprocessing.cpu_count()
 loglevel = "info"
-proc_name = "curlbin"
+proc_name = "{{cookiecutter.project_name}}"
 timeout = 1200
