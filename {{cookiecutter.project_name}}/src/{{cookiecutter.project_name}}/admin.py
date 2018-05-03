9387230a08bdbd33fe97ac3a8196424f12cdfa5d
daijian@air <daijian@flowself.com>
2018-05-03 20:06:17 +0800

    fix bugs

--- {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/admin.py
+++ {{cookiecutter.project_name}}/src/{{cookiecutter.project_name}}/admin.py
@@ -3,6 +3,6 @@ from . import models as m
 
 
 # Register your models here.
-@admin.register(m.PathRule)
-class PathRuleAdmin(admin.ModelAdmin):
-    list_display = [f.name for f in m.PathRule._meta.get_fields()]
+# @admin.register(m.PathRule)
+# class PathRuleAdmin(admin.ModelAdmin):
+#     list_display = [f.name for f in m.PathRule._meta.get_fields()]
