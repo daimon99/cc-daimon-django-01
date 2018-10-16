056908a76d51ce7db32d631a190d815f6eaf9d67
daijian@air <daijian@flowself.com>
2018-10-16 11:03:01 +0800

    add restframework

--- {{cookiecutter.project_name}}/src/config/urls.py
+++ {{cookiecutter.project_name}}/src/config/urls.py
@@ -15,6 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+from rest_framework.documentation import include_docs_urls
+from rest_framework_jwt.views import obtain_jwt_token, verify_jwt_token, refresh_jwt_token
+
 
 urlpatterns = [
     path('', RedirectView.as_view(url='/admin/', permanent=False)),
